d3dc554359d4f8ec50a9fc15cc98d0c5788bc928
manish <myadav.dce@gmail.com>
2023-02-05 22:36:59 +0000

    Adding sample Android activity

---
+++ app/src/main/java/com/spark/fastplayer/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.spark.fastplayer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.spark.fastplayer.ui.theme.FastPlayerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FastPlayerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Fast Player")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    FastPlayerTheme {
+        Greeting("Android")
+    }
+}
