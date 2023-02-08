a44d7a11fc03afc3d5dfb4c12c3767193bd70bac
Aakash <aakashchaturvedi7@gamil.com>
2023-02-08 20:03:50 +0100

    task_1_2_3// added new splash UI, hilt setup

--- app/src/main/java/com/spark/fastplayer/MainActivity.kt
+++ app/src/main/java/com/spark/fastplayer/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.spark.fastplayer
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.ViewTreeObserver
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,11 +15,25 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.spark.fastplayer.presentation.splash.SplashState
+import com.spark.fastplayer.presentation.splash.SplashViewModel
 import com.spark.fastplayer.ui.theme.FastPlayerTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: SplashViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.splashState.value == SplashState.Loaded
+            }
+        }
+
         setContent {
             FastPlayerTheme {
                 // A surface container using the 'background' color from the theme
---
+++ app/src/main/java/com/spark/fastplayer/presentation/splash/SplashViewModel.kt
@@ -0,0 +1,31 @@
+package com.spark.fastplayer.presentation.splash
+
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.spark.fastplayer.common.CoroutineContextProvider
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SplashViewModel @Inject constructor(coroutineContextProvider: CoroutineContextProvider)  : ViewModel() {
+
+    private val _splashState = MutableStateFlow<SplashState>(SplashState.Init)
+    val splashState = _splashState.asStateFlow()
+
+    init {
+        viewModelScope.launch(coroutineContextProvider.io) {
+            delay(SPLASH_DELAY_TIMER)
+            _splashState.value = SplashState.Loaded
+        }
+    }
+
+    companion object {
+        private const val SPLASH_DELAY_TIMER = 3000L
+    }
+
+}
